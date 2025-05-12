458a241287ec2b9e54ffdf5d22f94f32f2b47585
davidnato <wanyamanato254@gmail.com>
2025-05-12 15:02:31 +0300

    Tried the read more and read less feature

--- js/main.js
+++ js/main.js
@@ -362,3 +362,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const hoverLogo = new Image();
     hoverLogo.src = 'images/logo-anim.gif';
 });
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
